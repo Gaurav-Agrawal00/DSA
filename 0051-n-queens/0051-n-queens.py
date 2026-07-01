cdea9b3eccc9c875bd1f1c604059e7d4e6c5b698
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-07-01 12:42:01 +0530

    Time: 7 ms (96.55%), Space: 19.8 MB (9.63%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,46 +1,75 @@
+# class Solution:
+
+    # def isSafe(self,board,row,col,n):
+    #     nrow = row
+    #     ncol = col - 1
+    #     while ncol >= 0:
+    #         if board[nrow][ncol] == 'Q':
+    #             return False
+    #         ncol -= 1
+        
+    #     nrow = row-1
+    #     ncol = col -1
+    #     while(nrow >= 0 and ncol >= 0):
+    #         if board[nrow][ncol] == 'Q':
+    #             return False
+    #         ncol -= 1
+    #         nrow -= 1
+
+    #     nrow = row + 1
+    #     ncol = col -1
+    #     while(nrow < n and ncol >= 0):
+    #         if board[nrow][ncol] == 'Q':
+    #             return False
+    #         ncol -= 1
+    #         nrow += 1
+    #     return True
+
+    # def findNQueens(self,col,board,ans,n):
+    #     if col == n:
+    #         temp = [''.join(row) for row in board]
+    #         ans.append(temp)
+    #         return
+        
+    #     for row in range(n):
+    #         if self.isSafe(board,row,col,n) :
+    #             board[row][col] = 'Q'
+    #             self.findNQueens(col+1,board,ans,n)
+    #             board[row][col] = '.'
+                
+    # def solveNQueens(self, n: int) -> List[List[str]]:
+        # board = [['.' for i in range(n)]for j in range(n)]
+        # ans = []
+        # self.findNQueens(0,board,ans,n)
+        # return ans
+
 class Solution:
 
-    def isSafe(self,board,row,col,n):
-        nrow = row
-        ncol = col - 1
-        while ncol >= 0:
-            if board[nrow][ncol] == 'Q':
-                return False
-            ncol -= 1
-        
-        nrow = row-1
-        ncol = col -1
-        while(nrow >= 0 and ncol >= 0):
-            if board[nrow][ncol] == 'Q':
-                return False
-            ncol -= 1
-            nrow -= 1
-
-        nrow = row + 1
-        ncol = col -1
-        while(nrow < n and ncol >= 0):
-            if board[nrow][ncol] == 'Q':
-                return False
-            ncol -= 1
-            nrow += 1
-        return True
-
-
-    def findNQueens(self,col,board,ans,n):
+    def findNQueens(self,col,board,n,ans,leftRow,upperDiagonal,lowerDiagonal):
         if col == n:
             temp = [''.join(row) for row in board]
             ans.append(temp)
             return
         
         for row in range(n):
-            if self.isSafe(board,row,col,n) :
+            if leftRow[row] == False and lowerDiagonal[row+col] == False and upperDiagonal[n-1+col-row] == False:
+                leftRow[row] = True
+                lowerDiagonal[row+col] = True
+                upperDiagonal[n-1+col-row] = True
+
                 board[row][col] = 'Q'
-                self.findNQueens(col+1,board,ans,n)
+                self.findNQueens(col+1,board,n,ans,leftRow,upperDiagonal,lowerDiagonal)
+                leftRow[row] = False
+                lowerDiagonal[row+col] =False
+                upperDiagonal[n-1+col-row] = False
                 board[row][col] = '.'
-                
+
 
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [['.' for i in range(n)]for j in range(n)]
+        board = [['.' for i in range(n)] for i in range(n)]
+        leftRow  = [False]*n
+        upperDiagonal = [False]*(2*n-1)
+        lowerDiagonal = [False]*(2*n-1)
         ans = []
-        self.findNQueens(0,board,ans,n)
+        self.findNQueens(0,board,n,ans,leftRow,upperDiagonal,lowerDiagonal)
         return ans
