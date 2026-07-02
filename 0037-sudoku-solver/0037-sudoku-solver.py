08d43b4eb332790ba12ecda14b9cfa1a52196e28
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-07-02 17:55:24 +0530

    Time: 49 ms (96.77%), Space: 19.3 MB (98.52%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,61 +1,66 @@
 class Solution:
-    def solveSudoku(self, board) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        # N-Queens wala Hashing Magic! 
-        # Sets use kar rahe hain taaki O(1) mein check kar sakein
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-        empty_cells = []
-        
-        # STEP 1: Pura board scan karo. 
-        # Jo number pehle se hain, unhe Sets mein daal do.
-        # Jo khali hain, unhe empty_cells mein daal do.
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        rows = [[0]*9 for _ in range(9)]
+        cols = [[0]*9 for _ in range(9)]
+        grids = [[0]*9 for _ in range(9)]
+
+        # initial scan
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != '.':
+                    num = int(board[r][c]) - 1
+                    g = (r // 3) * 3 + (c // 3)
+                    rows[r][num] = 1
+                    cols[c][num] = 1
+                    grids[g][num] = 1
+
+        # build hash table of empty cells with their candidates
+        candidates = {}
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
-                    empty_cells.append((r, c))
-                else:
-                    val = board[r][c]
-                    box_idx = (r // 3) * 3 + (c // 3)
-                    
-                    # Sets mein number daal diya
-                    rows[r].add(val)
-                    cols[c].add(val)
-                    boxes[box_idx].add(val)
-                    
-        # STEP 2: Main Backtracking Function
-        def backtrack(index):
-            # Base Case
-            if index == len(empty_cells):
+                    g = (r // 3) * 3 + (c // 3)
+                    valid = []
+                    for num in range(9):
+                        if rows[r][num] == 0 and cols[c][num] == 0 and grids[g][num] == 0:
+                            valid.append(num)
+                    candidates[(r, c)] = valid
+
+        def backtrack():
+            if not candidates:
                 return True
-                
-            r, c = empty_cells[index]
-            box_idx = (r // 3) * 3 + (c // 3)
-            
-            for val in "123456789":
-                # O(1) CHECK - Bina kisi function ya loop ke!
-                if val not in rows[r] and val not in cols[c] and val not in boxes[box_idx]:
-                    
-                    # Tika lagao (Board par aur Sets dono mein)
-                    board[r][c] = val
-                    rows[r].add(val)
-                    cols[c].add(val)
-                    boxes[box_idx].add(val)
-                    
-                    # Recursion
-                    if backtrack(index + 1) == True:
-                        return True
-                        
-                    # Backtrack (Tika hatao)
-                    board[r][c] = '.'
-                    rows[r].remove(val)
-                    cols[c].remove(val)
-                    boxes[box_idx].remove(val)
-                    
+
+            cell = min(candidates, key=lambda k: len(candidates[k]))
+            r, c = cell
+            g = (r // 3) * 3 + (c // 3)
+            options = candidates.pop(cell)
+
+            for num in options:
+                rows[r][num] = 1
+                cols[c][num] = 1
+                grids[g][num] = 1
+                board[r][c] = str(num + 1)
+
+                affected = {}
+                for key in list(candidates):
+                    kr, kc = key
+                    kg = (kr // 3) * 3 + (kc // 3)
+                    if kr == r or kc == c or kg == g:
+                        if num in candidates[key]:
+                            candidates[key].remove(num)
+                            affected[key] = num
+
+                if backtrack():
+                    return True
+
+                rows[r][num] = 0
+                cols[c][num] = 0
+                grids[g][num] = 0
+                board[r][c] = '.'
+                for key, n in affected.items():
+                    candidates[key].append(n)
+
+            candidates[cell] = options
             return False
-            
-        # Function ko call karo
-        backtrack(0)
+
+        backtrack()
