cdabdbd8a46af04cd5c3ed50f06086c9d18c59f5
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-07-05 12:46:39 +0530

    Time: 4 ms (7.87%), Space: 19.2 MB (70.95%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def find(self,ind,k,target,ds,ans):
+        if target == 0 and k == len(ds):
+            ans.append(list(ds))
+            return
+
+        for i in range(ind,10):
+            if ind > target:
+                break
+
+            ds.append(i)
+
+            self.find(i+1 , k , target - i , ds , ans)
+            ds.pop()
+
+
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ds = []
+        ans = []
+        self.find(1,k,n,ds,ans)
+        return ans
