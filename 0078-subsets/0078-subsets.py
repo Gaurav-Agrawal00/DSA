b888e892c71096a55a7ad0eaa8919ada7767ce4c
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-07-08 11:57:28 +0530

    Time: 1 ms (10.38%), Space: 19.2 MB (91.85%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,32 @@
 class Solution:
 
-    def findSubsets(self,ind,arr,ans,ds):
-        if ind == len(arr) :
-            ans.append(list(ds))
-            return
+    # def findSubsets(self,ind,arr,ans,ds):
+    #     if ind == len(arr) :
+    #         ans.append(list(ds))
+    #         return
         
-        ds.append(arr[ind])
-        self.findSubsets(ind+1,arr,ans,ds)
-        ds.pop()
+    #     ds.append(arr[ind])
+    #     self.findSubsets(ind+1,arr,ans,ds)
+    #     ds.pop()
 
-        self.findSubsets(ind+1,arr,ans,ds)
+    #     self.findSubsets(ind+1,arr,ans,ds)
 
         
 
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     ans = []
+    #     ds = []
+    #     self.findSubsets(0,nums,ans,ds)
+    #     return ans
+
+        n = len(nums)
+        subsets = 1 << n
         ans = []
-        ds = []
-        self.findSubsets(0,nums,ans,ds)
-        return ans
+        for num in range(subsets):
+            ds = []
+            for j in range(n):
+                if num & (1<<j) :
+                    ds.append(nums[j])
+            ans.append(ds)
+        return ans 
