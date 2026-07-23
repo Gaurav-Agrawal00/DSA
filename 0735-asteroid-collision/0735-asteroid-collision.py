4e494dfb68f03c4ae91f047681895c158f4e06f5
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-07-23 16:00:48 +0530

    Time: 7 ms (58.12%), Space: 20.4 MB (31.52%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -6,7 +6,7 @@ class Solution:
         for val in asteroids:
             if val < 0 :
                 if stack and stack[-1] > 0:
-                    while stack and stack[-1] < abs(val) and stack[-1] > 0:
+                    while stack and stack[-1] > 0 and stack[-1] < abs(val) :
                         stack.pop()
                     if stack and stack[-1] == abs(val) :
                         stack.pop()
