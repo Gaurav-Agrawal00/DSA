eb8b663412de0ef0af0ef7f52ac178c42e552026
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-07-23 15:59:18 +0530

    Time: 3 ms (93.72%), Space: 20.4 MB (7.75%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        
+        for val in asteroids:
+            if val < 0 :
+                if stack and stack[-1] > 0:
+                    while stack and stack[-1] < abs(val) and stack[-1] > 0:
+                        stack.pop()
+                    if stack and stack[-1] == abs(val) :
+                        stack.pop()
+                        continue
+                    if len(stack) == 0 or stack[-1] < 0:
+                        stack.append(val)
+                else:
+                    stack.append(val)
+            else:
+                stack.append(val)
+        return stack
