c95c68d1d597934232b2f1f86e76078aa06d45c1
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-08-06 19:58:12 +0530

    Time: 168 ms (85.04%), Space: 26 MB (34.05%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,29 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        a = -1
-        b = -1 
-        length = 0
-        maxCnt = 0
-        l = 0 
-        l1 = 0
+        l = 0
         r = 0
+        maxLen = 0
+        ansSet = {}
         while r < len(fruits):
-            # 1. Agar naya fruit aaya jo dono baskets mein nahi hai
-            if fruits[r] != a and fruits[r] != b:
-                if a < 0:
-                    a = fruits[r]
-                elif b < 0:
-                    b = fruits[r]
-                else:
-                    l = l1
-                    a = fruits[r-1]
-                    b = fruits[r]
-            
-            # 2. 'l1' hamesha aakhiri continuous same fruits ke block ka index hona chahiye
-            if r > 0 and fruits[r] != fruits[r-1]:
-                l1 = r
-                
-            length = r - l + 1
-            maxCnt = max(length, maxCnt)
+            ansSet[fruits[r]] = ansSet.get(fruits[r],0) + 1
+            if len(ansSet) > 2 :
+                ansSet[fruits[l]] -= 1
+                if ansSet[fruits[l]] == 0:
+                    del ansSet[fruits[l]]
+                l += 1
+            maxLen = max(maxLen,r-l+1)
             r += 1
-        return maxCnt
+        return maxLen
