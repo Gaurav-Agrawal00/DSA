c1ea5cccff8cc2d190465c1f0d151afe10abf030
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-08-13 04:16:40 +0530

    Time: 580 ms (5.03%), Space: 17.4 MB (27.67%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     int longestSubstring(string s, int k) {
         int n =s.length();
-        int i=0;
-        // unordered_map<char,int> mymap;
         int ans=0;
 
-        for (i=0; i<n ; i++){
+        for (int i=0; i<n ; i++){
             unordered_map<char,int> mymap;
+            int count=0;
             for (int j=i; j<n ; j++){
                 mymap[s[j]]++;
 
-                bool ok=true;
-                for (auto [key,val]:mymap){
-                    if (!(val>=k)){
-                        ok=false;
-                    }
+                if (mymap[s[j]]==k){
+                    count++;
                 }
 
-                if (ok){
-                    ans=max(ans,j-i+1);
-                }
+                if (count==mymap.size()) ans=max(ans,j-i+1);
                 
             }
         }
