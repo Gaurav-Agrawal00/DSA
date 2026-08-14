d5d0a1bb9adc338a3d0d64c2e4ebe08742f301b1
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-08-14 15:29:53 +0530

    Time: 3 ms (79.79%), Space: 19.2 MB (88.98%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,13 +2,13 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         romanSet = {"I" : 1 , "V" : 5 , "X" : 10 , "L" : 50 , "C" : 100 , "D" : 500 , "M" : 1000}
         i = ans = 0
-        while i < len(s) - 1:
-            if romanSet[s[i]] < romanSet[s[i+1]]:
+        while i < len(s):
+            if i < len(s)-1 and romanSet[s[i]] < romanSet[s[i+1]]:
                 ans += romanSet[s[i+1]] - romanSet[s[i]]
                 i += 2
             else:
                 ans+= romanSet[s[i]]
                 i += 1
-        if romanSet[s[len(s)-1]] <= romanSet[s[len(s)-2]]:
-            ans += romanSet[s[i]]
+        # if romanSet[s[len(s)-1]] <= romanSet[s[len(s)-2]]:
+        #     ans += romanSet[s[i]]
         return ans
