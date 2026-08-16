6129aa8f4b5a57dda898232351e91cc7639349e4
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-08-16 17:56:18 +0530

    Time: 0 ms (100%), Space: 23.4 MB (39.2%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,23 +1,25 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        hash_map = {}
+        five = 0
+        ten = 0
+        
         for b in bills:
-            hash_map[b] = hash_map.get(b , 0) + 1 
             if b == 5:
-                continue
-            elif b == 10 :
-                if hash_map.get(5,0) > 0 :
-                    hash_map[5] -= 1
+                five += 1
+            elif b == 10:
+                if five > 0:
+                    five -= 1
+                    ten += 1
                 else:
                     return False
-            else:
-                if  hash_map.get(10,0) > 0 and  hash_map.get(5,0) > 0 :
-                    hash_map[10] -= 1
-                    hash_map[5] -= 1
-
-                elif  hash_map.get(5,0) >= 3:
-                    hash_map[5] -= 3
+            else: # b == 20
+                # Hamesha greedy bano: pehle $10 ka note nikalne ki koshish karo
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
                     
-                else:
-                    return False
         return True
