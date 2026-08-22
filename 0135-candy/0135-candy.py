e5fce072e1fac1389978b42a5da121ace8226414
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-08-22 16:01:22 +0530

    Time: 46 ms (15.14%), Space: 25.1 MB (15.05%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,37 +1,25 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        sum1 = 1
         n = len(ratings)
-        if n == 1:
-            return 1
-        
-        candy = [1] * n
-        for i in range(1,n):
-            if ratings[i-1] < ratings[i]:
-                candy[i] = candy[i-1] + 1
-        
-        for i in range(n-2,-1,-1):
-            if ratings[i+1 ] < ratings[i] :
-                candy[i] = max(candy[i+1] + 1 , candy[i])
-                
-        return sum(candy)
-
-
-
-        # specialcase if rearrangement is possible
-        # n = len(ratings)
-        # if n == 1:
-        #     return 1
-        # ratings.sort()
-        # l = 0
-        # r = n - 1
-        # cnt = 0
-        # while l < r:
-        #     if ratings[l] < ratings[r]:
-        #         cnt += 3
-        #         l += 1
-        #         r -= 1
-        #     else :
-        #         break
-        # if l <= r:
-        #     cnt += r - l + 1
-        # return cnt
+        i = 1
+        while i < n:
+            if ratings[i] == ratings[i-1]:
+                sum1 += 1
+                i += 1
+                continue
+            
+            peak = 1
+            while i < n and ratings[i] > ratings[i-1]:
+                peak += 1
+                sum1 += peak 
+                i += 1
+            
+            down = 1
+            while i < n and ratings[i] < ratings[i - 1]:
+                sum1 += down 
+                i += 1
+                down += 1
+            if down > peak :
+                sum1 += down - peak
+        return sum1
