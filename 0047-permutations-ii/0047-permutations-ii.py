702738a92bf8730a48b68ba0ca7c2255d2a6cf91
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-08-27 20:41:56 +0530

    Time: 3 ms (90.23%), Space: 19.4 MB (93.58%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,14 +1,24 @@
 class Solution:
-    def findPer(self,ind,nums,ans,n):
+    def findPer(self, ind, nums, ans, n):
         if ind == n:
-            ans.add(tuple(nums[:]))
+            ans.append(nums[:])
+            return 
         
-        for i in range(ind,n):
-            nums[i] , nums[ind] = nums[ind] , nums[i]
-            self.findPer(ind+1 , nums,ans,n)
-            nums[i] , nums[ind] = nums[ind] , nums[i]
+        # This set keeps track of the numbers we've swapped into 'ind' 
+        # for THIS specific depth of the recursion tree.
+        seen = set()
+        
+        for i in range(ind, n):
+            if nums[i] in seen:
+                continue # Skip this branch, we've already done this number
+            
+            seen.add(nums[i])
+            
+            nums[i], nums[ind] = nums[ind], nums[i]
+            self.findPer(ind + 1, nums, ans, n)
+            nums[i], nums[ind] = nums[ind], nums[i]
 
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
-        self.findPer(0,nums,ans,len(nums))
-        return list(ans)
+    def permuteUnique(self, nums: list[int]) -> list[list[int]]:
+        ans = [] # We can go back to using a normal list now!
+        self.findPer(0, nums, ans, len(nums))
+        return ans
