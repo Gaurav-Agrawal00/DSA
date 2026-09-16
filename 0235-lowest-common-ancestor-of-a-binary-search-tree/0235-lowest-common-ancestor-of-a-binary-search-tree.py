706cbe16121335272524f4dccc77ac566702c890
Gaurav kumar <gauravkumaragarwal7890@gmail.com>
2026-09-16 13:16:52 +0530

    Time: 64 ms (60.18%), Space: 22.9 MB (18.64%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,20 +6,11 @@
 #         self.right = None
 
 class Solution:
-    def findLca(self,node,p,q):
-        # if node.val == q.val:
-        if not node:
-            return
+    def lowestCommonAncestor(self, node: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        if node.val > q.val:
-            return self.findLca(node.left,p,q)
-        elif node.val <= q.val and node.val >= p.val:
-            return node
+        if node.val > q.val and node.val > p.val:
+            return self.lowestCommonAncestor(node.left,p,q)
+        elif node.val < q.val and node.val < p.val:
+            return self.lowestCommonAncestor(node.right,p,q)
         else:
-            return self.findLca(node.right,p,q)
-
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if p.val > q.val:
-            return self.lowestCommonAncestor(root,q,p)
-        
-        return self.findLca(root,p,q)
+            return node
